8d4fb5d42af1d0c2998ab500e9906c7991963e06
Karthik BT <79859147+karthikdiscoveries@users.noreply.github.com>
2021-09-16 10:13:28 +0530

    Initial commit

---
+++ src/pages/home-page/home-page-component.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './home-page-component.css';
+
+const HomePage = () =>{
+    return (
+        <div className="home-page">
+            <p>This is the HomePage</p>
+
+        </div>
+    )
+}
+
+export default HomePage;
