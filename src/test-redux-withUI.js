e841c253c5d5453e0e567beacb27ea5fddd4224f
Karthik BT <79859147+karthikdiscoveries@users.noreply.github.com>
2021-09-20 10:27:17 +0530

    css for rotate function

--- src/test-redux-withUI.js
+++ src/test-redux-withUI.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './test-reduxUI.scss';
+
 
 //higher order components
 
@@ -9,12 +9,13 @@ import {connect} from 'react-redux';
 //Import Actions
 import {firstAction} from './actions/actionOne';
 import { secondAction } from './actions/actionTwo';
-
+import './test-reduxUI.css';
 class rotateFunction extends React.Component {
     render(){
         return(
             <div className="rotateFunction">
-                <img src={logo}  alt="images" className={this.props.rotate? "n":"App-logo-paused"} 
+                <img src={logo}  alt="images" 
+                className={"App-logo" + (this.props.rotate? "":"App-logo-paused")}
                 onClick={this.props.rotate ? this.props.stopAction : this.props.startAction }/>
             </div>
         )
